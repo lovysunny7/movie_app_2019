21549fdbb3c2b3d7bcf4d5c91e3e68cd0119bdeb
lovysunny <lovysunny7@daum.net>
2020-02-12 17:03:17 +0900

    State 활용하기
    1. class 내부에 state를 선언하고 onClick 동작에 state와 관련된 함수를
    호출하면 render로 즉각적으로 화면을 바꿀 수 있다.
    2. state는 간단히 이와 같이 만들면 된다.
    state ={
    count : 0
    // 변수명 : 변수값 형태로 state = {} 내부에 나열하면 된다.
    }
    3. 함수는 아래와 같은 형태로 만들면 된다.
    동적으로 state를 render하기 위해서는 setState 함수를 사용해야한다.
    add = () {
    this.setState(current=>({count:current.count+1}));
    }
    4. 그리고 render() 내부에서는
    state를 나타내는 값은 {this.state.count}로 나타내고,
    state를 변경하기 위해 button에 onClick 이벤트를 변경하기 위해서는
    <button onClick={this.add}>Add</button> 형식으로 진행된다.
    5. 앞으로 더 나은 방법이 나올 수 있으나, 숙지하자!

--- src/App.js
+++ src/App.js
@@ -4,82 +4,41 @@ import PropTypes from "prop-types";
 // ./ 의 의미는 같은 디렉토리에 있다는 뜻이다.
 // function Food(props.fav)는 function Food ({fav})를 쓴 것과 동일하다.
 // map takes array and returns array way the what we want
+// *.propTypes로 이름을 명명해야지, 체크 가능함
+// {/* Food Component에서 fav(favoirte)라는 prop에 kimchi라는 value를 준 것 */}
+// {/* <Food fav="kimchi" /> */}
+/*
+ 왜 function App 도 있는데, class App extends React.Component를 사용해야 하는가?
+ 그것은 바로 state 라는 기능을 사용할 수 있기 때문이다.
+ state는 component의 data가 변하게 할 수 있다.
 
-function Food({ name, image, rating }) {
-  return (
-    <div>
-      <h2> I love {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={image} alt={name} />
-    </div>
-  );
-}
+ */
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+  // 아래는 자바 스크립트 코드
+  add = () => {
+    // console.log("add");
+    // this.state.count = 1; 이렇게 하게 되면 render을 다시 하지 않기 때문에 setState를 사용해야한다.
+    // 사실 이와 같은 방법은 좋은 방법으로 추천되진 않는다.
+    // this.setState({ count: this.state.count + 1 })
+    // this.setState(current=>({count:current.count+1})) 라고 바꿀 수도 있다.
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState(current => ({ count: current.count - 1 }));
+  };
 
-const foodLove = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FeTucCw1w6Ak%2Fmaxresdefault.jpg&f=1&nofb=1",
-    rating: 4.3
-  },
-  {
-    id: 2,
-    name: "Bibimbap",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2F6QQ67F8y2b8%2Fmaxresdefault.jpg&f=1&nofb=1",
-    rating: 5.0
-  },
-  {
-    id: 3,
-    name: "Doncasu",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Ffoodingbox.com%2Fweb%2Fproduct%2Fbig%2F201706%2F18_shop1_970227.jpg&f=1&nofb=1",
-    rating: 4.9
-  },
-  {
-    id: 4,
-    name: "Kimbap",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F5%2F56%2FVegetable_gimbap.jpg%2F1200px-Vegetable_gimbap.jpg&f=1&nofb=1",
-    rating: 4.7
-  },
-  {
-    id: 5,
-    name: "Samkiopsal",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F99663B335A22340228&f=1&nofb=1",
-    rating: 5.0
+  render() {
+    return (
+      <div>
+        <h1>The number is {this.state.count}. </h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Monus</button>
+      </div>
+    );
   }
-];
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number
-};
-
-function renderFood(dish) {
-  //console.log(dish);
-  return (
-    <Food
-      key={dish.id}
-      name={dish.name}
-      image={dish.image}
-      rating={dish.rating}
-    />
-  );
-}
-
-function App() {
-  return (
-    <div>
-      <h1>Hello!</h1>
-      {/* Food Component에서 fav(favoirte)라는 prop에 kimchi라는 value를 준 것 */}
-      {/* <Food fav="kimchi" /> */}
-      {foodLove.map(renderFood)}
-    </div>
-  );
 }
 
 export default App;
